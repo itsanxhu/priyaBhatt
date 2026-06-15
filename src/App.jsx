230dee038c33e781ac2bcd35f9cf92f1d3f994b5
anshu <anshhufx@gmail.com>
2026-06-15 22:27:28 +0530

    profilePart-complete

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { MapPin, SunMedium, MoonStar, Monitor } from "lucide-react";
+import Experience from "./components/Experience";
+import { useState } from "react";
+
+const App = () => {
+  const [play, setPlay] = useState(false);
+  return (
+    <div className="max-w-4xl min-h-screen mx-auto border-x border pt-3 border-neutral-600/20">
+      {/* Header */}
+      <div className="h-18 flex justify-between items-center px-8">
+        <div className="flex items-center gap-1">
+          <MapPin size={18} className="text-neutral-500/70" />
+          <p className="text-neutral-500/70 tracking-tight leading-none text-sm">
+            Uttarakhand, India
+          </p>
+        </div>
+        <div className="flex items-center py-1 border px-1 gap-1 border-neutral-500/30 rounded-full">
+          <div className="h-6 w-6 rounded-full flex justify-center items-center bg-neutral-400/30">
+            <SunMedium size={16} />
+          </div>
+          <div className="h-6 w-6 rounded-full flex justify-center items-center hover:bg-neutral-400/30">
+            <Monitor size={16} strokeWidth={2} className="text-neutral-500" />
+          </div>
+          <div className="h-6 w-6 rounded-full flex justify-center items-center hover:bg-neutral-400/30">
+            <MoonStar size={16} strokeWidth={2} className="text-neutral-500" />
+          </div>
+        </div>
+      </div>
+      {/* Middle-part */}
+      <div className="h-75 w-full flex justify-center">
+        <div className="relative h-80 flex flex-col items-center max-w-md text-center gap-4">
+          <div
+            className="size-33 flex justify-center items-center cursor-pointer rounded-full border-2 border-dashed border-neutral-500/50 overflow-hidden"
+            onMouseEnter={() => setPlay(true)}
+            onMouseLeave={() => setPlay(false)}
+          >
+            <video
+              src="/video.mp4"
+              autoPlay
+              muted
+              loop
+              className={`absolute size-30 object-cover rounded-full transition-opacity duration-500 ${
+                play ? "opacity-100" : "opacity-0"
+              }`}
+            />
+
+            <img
+              src="/mainImage2.png"
+              alt=""
+              className={`absolute size-30 object-cover rounded-full transition-opacity duration-500 ${
+                play ? "opacity-0" : "opacity-100"
+              }`}
+            />
+          </div>
+
+          <div className="absolute right-10 top-24">
+            <p className="rounded-full tracking-tight bg-neutral-200/30 px-3 border-2 text-neutral-500 border-dashed py-1 text-xs border-neutral-600/20 flex justify-center items-center gap-1">
+              <div className="h-3 w-3 bg-green-300/40 rounded-full flex justify-center items-center">
+                <div className="h-1.5 w-1.5 bg-green-400 rounded-full"></div>
+              </div>
+              Open to Work
+            </p>
+          </div>
+          <div className="absolute left-0 top-10">
+            <p className="rounded-full tracking-tight bg-neutral-200/30 px-3 border-2 text-neutral-500 border-dashed py-1 text-xs border-neutral-600/20 flex justify-center items-center gap-1">
+              <div className="h-3 w-3 bg-orange-300/40 rounded-full flex justify-center items-center">
+                <div className="h-1.5 w-1.5 bg-orange-400 rounded-full"></div>
+              </div>
+              Product Designer
+            </p>
+          </div>
+          <h1
+            className="text-4xl tracking-tight namefont italic cursor-pointer text-neutral-900"
+            onMouseEnter={() => setPlay(true)}
+            onMouseLeave={() => setPlay(false)}
+          >
+            Priya Bhatt
+          </h1>
+          <p className="text-neutral-500/70 tracking-tight leading-5">
+            Currently working at SAY Design Studio. Once I start something I
+            love, I finish it. love, I finish it.
+          </p>
+        </div>
+      </div>
+      <Experience />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,16 @@
+const Experience = () => {
+  return (
+    <div className="h-80 px-8">
+      <p>EXPERIENCE</p>
+      <div className="flex justify-between">
+        <div className="flex items-center">
+          <img className="w-8" src="/ex1.png" alt="" />
+          <h1>SAY Design Studio ・ Product Designer</h1>
+        </div>
+        <p className="text-center col-span-5">Dec 2023 - Present</p>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
